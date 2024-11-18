79b01de8d8a4f2bfc72acfdea48a06da3daed486
youoyo000 <angela0978017842@gmail.com>
2024-11-18 17:02:56 +0800

    v2

--- app/src/main/java/com/example/game2d/Game.kt
+++ app/src/main/java/com/example/game2d/Game.kt
@@ -6,15 +6,23 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 
-class Game(val scope: CoroutineScope) {
+class Game(val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
     var counter = 0
     val state = MutableStateFlow(0)
+    val background = Background(screenW)
+
+    var isPlaying = true
 
     fun Play(){
         scope.launch {
-            counter = 0
-            while (counter<1000) {
+          //  counter = 0
+            isPlaying = true
+
+            while (isPlaying) {
                 delay(40)
+                background.Roll()
+
+
                 counter++
                 state.emit(counter)
             }
--- app/src/main/java/com/example/game2d/MainActivity.kt
+++ app/src/main/java/com/example/game2d/MainActivity.kt
@@ -2,19 +2,16 @@ package com.example.game2d
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.provider.Settings.Global
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.game2d.ui.theme.Game2DTheme
 
 import androidx.compose.runtime.getValue
@@ -29,8 +26,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -40,10 +35,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Game2DTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val game = Game(GlobalScope)
-                    Start(m = Modifier.padding(innerPadding),game
-                    )
+
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+                    val screenW = resources.displayMetrics.widthPixels
+                    val screenH = resources.displayMetrics.heightPixels
+                    val game = Game(GlobalScope,screenH,screenW)
+                    Start(m = Modifier.padding(innerPadding), game)
 
                 }
             }
@@ -52,30 +49,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Start(m:Modifier,game: Game ){
+fun Start(m: Modifier, game: Game){
     val counter by game.state.collectAsState()
     var counter2 by remember { mutableStateOf(0) }
+    var msg by remember { mutableStateOf("遊戲開始") }
+
 
     Image(
         painter = painterResource(id = R.drawable.forest),
         contentDescription = "背景圖",
         contentScale = ContentScale.FillBounds,  //縮放符合螢幕寬度
         modifier = Modifier
-            .offset { IntOffset(-counter, 0) }
+            .offset { IntOffset(game.background.x1, 0) }
     )
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖2",
+        contentScale = ContentScale.FillBounds,  //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(game.background.x2, 0) }
+    )
+
 
 
     Row {
         Button(
             onClick = {
-                game.Play()
+                if (msg=="遊戲開始"){
+                    msg = "遊戲暫停"
+                    game.Play()
+                }
+                else{
+                    msg = "遊戲開始"
+                    game.isPlaying = false
+                }
+
             },
             modifier = m
         ) {
-            Text(text = "開始")
+            Text(text = msg)
+
         }
 
-        Text(text= counter.toString(), modifier = m)
+        Text(text = "%.2f 秒".format(counter*.04), modifier = m)
+
 
         Button(
             onClick = {
